8397cfb897fbd719a92b5a088ad19f17ad72c313
ssghost <ssprof0@gmail.com>
2022-04-25 13:19:09 +0800

    modify converter

--- convert.py
+++ convert.py
@@ -5,7 +5,7 @@ import datetime
 my_transformer = Transformer()
 current_time = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 my_model_path = "./model_"+current_time
-my_model = my_transformer.create_transformer().save(my_model_path)
+my_model = my_transformer.train().save(my_model_path)
 tfjs.converters.save_keras_model(my_model, my_model_path+"_converted/")
 
 
