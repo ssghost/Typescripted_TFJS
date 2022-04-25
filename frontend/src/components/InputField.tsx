c1a7a48fc0376470d8020248c59145a86d3fb53c
ssghost <ssprof0@gmail.com>
2022-04-25 13:16:59 +0800

    add components

---
+++ frontend/src/components/InputField.tsx
@@ -0,0 +1,36 @@
+import React, { PropsWithChildren, useRef } from "react";
+import "./styles.css";
+
+interface props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<props> = ({ que, setQue, handleAdd }:PropsWithChildren<any>) => {
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <form
+      className="input"
+      onSubmit={(e) => {
+        handleAdd(e);
+        inputRef.current?.blur();
+      }}
+    >
+      <input
+        type="text"
+        placeholder="Enter a Question."
+        value={que}
+        ref={inputRef}
+        onChange={(e) => setQue(e.target.value)}
+        className="input_box"
+      />
+      <button type="submit" className="input_submit">
+        ANSWER
+      </button>
+    </form>
+  );
+};
+
+export default InputField;
